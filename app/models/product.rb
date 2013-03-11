98dd157745c09da0b6da68a83b34a29aabeb7e38
Jon Dunleavy <jon.dunleavy@pennantplc.co.uk>
2013-03-11 21:32:16 +0000

    Updated product to have latest helper function

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,8 +11,7 @@ class Product < ActiveRecord::Base
         message: 'must be a URL for GIF, JPG or PNG image.'
       }
 
-end
-
-def self.latest
-  Product.order('updated_at').last
+  def self.latest
+    Product.order('updated_at').last
+  end
 end
